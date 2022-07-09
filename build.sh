92e0d57c4843d423f761042c01a3abfa1eab299c
Lily Scott <git@suchipi.tv>
2022-07-08 21:07:42 -0600

    Bump to latest quickjs version, use .a instead of all the .o files

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -ex
 
 tsc
 ./quickjs/build/src/qjsc/qjsc.target -e -o dist/tonna.c dist/index.js
-gcc -static -o dist/tonna dist/tonna.c quickjs/build/src/{cutils,libbf,libregexp,libunicode,quickjs,quickjs-libc}/*.target.o -Iquickjs/src/quickjs-libc -lm -lpthread -ldl
+gcc -static -o dist/tonna dist/tonna.c quickjs/build/src/archive/quickjs.target.a -Iquickjs/src/quickjs-libc -lm -lpthread -ldl
