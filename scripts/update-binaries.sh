97c1f513e13225e3485ff7df216bc631180de417
Lily Scott <me@suchipi.com>
2022-07-27 01:58:45 -0600

    Update macOS binary to 0.0.2

--- scripts/update-binaries.sh
+++ scripts/update-binaries.sh
@@ -6,5 +6,5 @@ if [ `uname` == "Linux" ]; then
   ./scripts/build.sh && cp ./dist/yavascript ./bin/linux/
   env WINDOWS=yes ./scripts/build.sh && cp ./dist/yavascript.exe ./bin/win32/
 else
-  ./scripts/build.sh && cp ./dist/yavascript ./darwin/linux/
+  ./scripts/build.sh && cp ./dist/yavascript ./bin/darwin/
 fi
