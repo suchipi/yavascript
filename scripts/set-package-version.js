a9114e344508ca45cad577f2e4db32f592494dc6
Lily Scott <git@suchipi.tv>
2022-08-02 21:51:13 -0600

    Add script for setting version in both package.jsons

---
+++ scripts/set-package-version.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env yavascript
+/// <reference path="../dist/yavascript.d.ts" />
+import "./lib/set-rootdir.js";
+
+const currentVersion = JSON.parse(readFile("./package.json")).version;
+console.log(`current version: ${currentVersion}`);
+
+const newVersion = scriptArgs[2];
+if (!newVersion) {
+  throw new Error("Please specify the new version.");
+}
+console.log(`new version: ${newVersion}`);
+
+const pkgJsonFiles = ["./package.json", "./npm/package.json"];
+
+for (const pkgJsonFile of pkgJsonFiles) {
+  const pkg = JSON.parse(readFile(pkgJsonFile));
+  pkg.version = newVersion;
+  writeFile(pkgJsonFile, JSON.stringify(pkg, null, 2) + "\n");
+  console.log(`updated ${pkgJsonFile}`);
+}
