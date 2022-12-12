99292b2f8480f9211f5d682879a09b5d8c4ff5cf
Lily Skye <me@suchipi.com>
2022-12-11 23:20:23 -0700

    remove obsolete backtrace stuff from runFileTarget

--- src/targets/run-file.ts
+++ src/targets/run-file.ts
@@ -1,26 +1,5 @@
 import * as std from "std";
 
 export default function runFileTarget(fileToRun: string) {
-  try {
-    std.importModule(fileToRun, "./<cwd>");
-  } catch (err) {
-    try {
-      // TODO: need backtraceBarrier for importModule.
-      // this is a workaround in the meantime.
-      const stackLines = (err as any).stack.split("\n");
-      const newLines: Array<string> = [];
-
-      for (const line of stackLines) {
-        if (/yavascript-internal\.js/.test(line)) {
-          break;
-        } else {
-          newLines.push(line);
-        }
-      }
-
-      (err as any).stack = newLines.join("\n");
-    } catch (err2) {}
-
-    throw err;
-  }
+  std.importModule(fileToRun, "./<cwd>");
 }
