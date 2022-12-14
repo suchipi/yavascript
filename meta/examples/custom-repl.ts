4452921efdb417043db2983bce66893543aee3a4
Lily Skye <me@suchipi.com>
2022-12-14 14:56:28 -0700

    actually, this is a more accurate name

--- meta/examples/custom-repl.ts
+++ meta/examples/custom-repl.ts
@@ -2,16 +2,16 @@
 /// <reference path="../../yavascript.d.ts" />
 
 // Just defining some made-up stuff for demonstration purposes...
-const TOKEN_REGEX = /[^\w]|_/g;
+const TOKEN_SEPARATOR = /[^\w]|_/g;
 function tokens(input: string) {
-  return input.split(TOKEN_REGEX).filter(Boolean);
+  return input.split(TOKEN_SEPARATOR).filter(Boolean);
 }
 
 // You can start the yavascript repl from your own file,
 // with your own stuff loaded, by calling `startRepl`:
 startRepl(
   // Variables to make available to the repl as globals. Optional
-  { TOKEN_REGEX, tokens },
+  { TOKEN_SEPARATOR, tokens },
   // Which language repl input should be interpreted as, ie. typescript, coffeescript, jsx, etc. Defaults to "javascript".
   "typescript"
 );
