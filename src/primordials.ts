7f6887ba6dbafaa93bf693e36e23bc28228ae675
Lily Skye <git@suchipi.tv>
2022-12-17 15:07:32 -0700

    rename _installApi.ts to _install-api.ts

--- src/primordials.ts
+++ src/primordials.ts
@@ -1,6 +1,6 @@
 // This file loads all the globals and etc
 
-import installApi from "./api/_installApi";
+import installApi from "./api/_install-api";
 installApi(globalThis);
 
 import "./extension-handlers/_load-all";
