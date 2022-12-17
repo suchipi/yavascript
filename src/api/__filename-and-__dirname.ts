d57ed2125144f83110cefda6b2261dd815a89377
Lily Skye <git@suchipi.tv>
2022-12-17 15:02:39 -0700

    __filename and __dirname: allow realpath to fail
    (in case it's not a real file, ie. using a fake path when eval-ing)

--- src/api/__filename-and-__dirname.ts
+++ src/api/__filename-and-__dirname.ts
@@ -4,11 +4,23 @@ import { dirname } from "./commands/dirname";
 
 // Not public API; exported for __filename, which *is* a public API
 export function get__filename(depth: number): string {
-  return os.realpath(std.getFileNameFromStack(depth));
+  let ret = std.getFileNameFromStack(depth);
+  try {
+    ret = os.realpath(ret);
+  } catch (err) {
+    // ignored
+  }
+  return ret;
 }
 
 // Not public API; exported for __dirname, which *is* a public API
 export function get__dirname(depth: number): string {
-  const filename = os.realpath(std.getFileNameFromStack(depth));
+  let filename = std.getFileNameFromStack(depth);
+  try {
+    filename = os.realpath(filename);
+  } catch (err) {
+    // ignored
+  }
+
   return dirname(filename);
 }
