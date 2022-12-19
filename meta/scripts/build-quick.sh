357166a315c77f4428669e773c4483d3a5689df4
Lily Skye <me@suchipi.com>
2022-12-19 13:42:12 -0700

    update comments in build-quick

--- meta/scripts/build-quick.sh
+++ meta/scripts/build-quick.sh
@@ -3,12 +3,12 @@ set -ex
 
 # faster (but less guaranteed to work) build script, for quick iteration.
 # only works on linux/darwin, and you need all the stuff from the docker
-# containers installed on the host machine (notably: node, gcc).
+# containers installed on the host machine (notably: node, gcc, ninja).
 
 # Move to repo root
 cd $(git rev-parse --show-toplevel)
 
-# Use node 17
+# Use node 18
 fnm use
 
 if [[ "$SKIP_QJS" == "" ]]; then
