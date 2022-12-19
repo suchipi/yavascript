309ea8eb954e24a0c5b5507ac5c191ef1cd5f2aa
Lily Skye <me@suchipi.com>
2022-12-19 15:40:18 -0700

    fix commands not being lazy-loaded properly

--- src/api/commands/_all.ts
+++ src/api/commands/_all.ts
@@ -3,17 +3,20 @@
 import { makeGetterPropertyDescriptorMap } from "../../lazy-load";
 
 export default makeGetterPropertyDescriptorMap({
-  basename: () => require("./basename"),
-  cat: () => require("./cat"),
-  cd: () => require("./cd"),
-  chmod: () => require("./chmod"),
-  dirname: () => require("./dirname"),
-  echo: () => require("./echo"),
-  extname: () => require("./extname"),
-  ls: () => require("./ls"),
-  printf: () => require("./printf"),
-  pwd: () => require("./pwd"),
-  readlink: () => require("./readlink"),
-  realpath: () => require("./realpath"),
-  touch: () => require("./touch"),
+  basename: () =>
+    (require("./basename") as typeof import("./basename")).basename,
+  cat: () => (require("./cat") as typeof import("./cat")).cat,
+  cd: () => (require("./cd") as typeof import("./cd")).cd,
+  chmod: () => (require("./chmod") as typeof import("./chmod")).chmod,
+  dirname: () => (require("./dirname") as typeof import("./dirname")).dirname,
+  echo: () => (require("./echo") as typeof import("./echo")).echo,
+  extname: () => (require("./extname") as typeof import("./extname")).extname,
+  ls: () => (require("./ls") as typeof import("./ls")).ls,
+  printf: () => (require("./printf") as typeof import("./printf")).printf,
+  pwd: () => (require("./pwd") as typeof import("./pwd")).pwd,
+  readlink: () =>
+    (require("./readlink") as typeof import("./readlink")).readlink,
+  realpath: () =>
+    (require("./realpath") as typeof import("./realpath")).realpath,
+  touch: () => (require("./touch") as typeof import("./touch")).touch,
 });
