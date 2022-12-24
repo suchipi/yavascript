c53b9659cab8cbf0645a67aadecad74b1487f250
Lily Skye <me@suchipi.com>
2022-12-24 02:53:40 -0700

    assemble dts with macaroni instead of m4

--- meta/scripts/assemble-dts.sh
+++ meta/scripts/assemble-dts.sh
@@ -3,12 +3,12 @@ set -ex
 
 mkdir -p dist
 
-M4="m4 -Isrc -Isrc/api -Isrc/api/commands -Isrc/api/exec -Isrc/templates -Imeta/quickjs/src/quickjs-libc"
+MACARONI="npx @suchipi/macaroni --include-paths src,src/api,src/api/commands,src/api/exec,src/templates,meta/quickjs/src/quickjs-libc"
 
-$M4 src/templates/yavascript.d.ts.tmpl > dist/yavascript.d.ts
+$MACARONI src/templates/yavascript.d.ts.tmpl > dist/yavascript.d.ts
 npx prettier --write dist/yavascript.d.ts
 
-$M4 src/templates/yavascript-git.d.ts.tmpl > yavascript.d.ts
+$MACARONI src/templates/yavascript-git.d.ts.tmpl > yavascript.d.ts
 npx prettier --write yavascript.d.ts
 
 # copy into npm folder
