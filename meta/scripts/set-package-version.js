63d79bd1b884cb9b6f5686c26b19496031fe0030
Lily Skye <me@suchipi.com>
2022-12-24 14:41:42 -0700

    refactor set-package-version a bit

--- meta/scripts/set-package-version.js
+++ meta/scripts/set-package-version.js
@@ -1,25 +1,44 @@
 #!/usr/bin/env yavascript
 /// <reference path="../../yavascript.d.ts" />
 
-cd(repoRoot());
+function main(newVersion) {
+  cd(repoRoot());
 
-const currentVersion = JSON.parse(readFile("./package.json")).version;
-console.log(`current version: ${currentVersion}`);
+  const currentVersion = readJson("./package.json").version;
+  console.log(`current version: ${currentVersion}`);
 
-const newVersion = scriptArgs[2];
-if (!newVersion) {
-  throw new Error("Please specify the new version.");
+  console.log(`new version: ${newVersion}`);
+
+  const pkgJsonFiles = ["./package.json", "./meta/npm/package.json"];
+
+  for (const pkgJsonFile of pkgJsonFiles) {
+    const pkg = readJson(pkgJsonFile);
+    pkg.version = newVersion;
+    writeJson(pkgJsonFile, pkg);
+    console.log(`updated ${pkgJsonFile}`);
+  }
+
+  // update package-lock.json
+  exec("npm install");
 }
-console.log(`new version: ${newVersion}`);
 
-const pkgJsonFiles = ["./package.json", "./meta/npm/package.json"];
-
-for (const pkgJsonFile of pkgJsonFiles) {
-  const pkg = JSON.parse(readFile(pkgJsonFile));
-  pkg.version = newVersion;
-  writeFile(pkgJsonFile, JSON.stringify(pkg, null, 2) + "\n");
-  console.log(`updated ${pkgJsonFile}`);
+function readJson(filepath) {
+  return JSON.parse(readFile(filepath));
 }
 
-// update package-lock.json
-exec("npm install");
+function writeJson(filepath, data) {
+  writeFile(filepath, JSON.stringify(data, null, 2) + "\n");
+}
+
+function readArgv() {
+  const { args } = parseScriptArgs();
+
+  const newVersion = args[0];
+  if (!newVersion) {
+    throw new Error("Please specify the new version.");
+  }
+
+  return [newVersion];
+}
+
+main(...readArgv());
