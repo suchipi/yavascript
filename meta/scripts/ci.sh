7874395dee174f089b2c7412a2164ae3fc19bd1d
Lily Skye <me@suchipi.com>
2022-12-27 14:21:47 -0700

    source nvm

--- meta/scripts/ci.sh
+++ meta/scripts/ci.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+# ugh, the gh actions image claims to have nvm, but they don't do this for you
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
 nvm use
 env SKIP_FNM_USE=1 meta/build.sh
