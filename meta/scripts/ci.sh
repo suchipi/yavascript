5d51cead1865b08b5ae3905a1add5f1b18e1de6b
Lily Skye <me@suchipi.com>
2022-12-27 14:26:31 -0700

    install ninja build in ci

--- meta/scripts/ci.sh
+++ meta/scripts/ci.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+sudo apt install ninja-build
+
 # ugh, the gh actions image claims to have nvm, but they don't do this for you
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
