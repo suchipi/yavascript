db7136cf6d90fa3a36e08474f8e7a124e6dc1498
Lily Skye <me@suchipi.com>
2022-12-27 14:24:35 -0700

    move set -x so we don't get noise from nvm, run clone-quickjs

--- meta/scripts/ci.sh
+++ meta/scripts/ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 # ugh, the gh actions image claims to have nvm, but they don't do this for you
 export NVM_DIR="$HOME/.nvm"
@@ -7,4 +7,8 @@ source "$NVM_DIR/nvm.sh"
 
 nvm install
 nvm use
+
+set -x
+
+meta/scripts/clone-quickjs.sh
 env SKIP_FNM_USE=1 meta/build.sh
