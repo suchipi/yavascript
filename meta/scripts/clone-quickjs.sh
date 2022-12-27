ad2e230a3807de61191f51ce3f930ba8f542da9e
Lily Skye <me@suchipi.com>
2022-12-27 14:02:17 -0700

    use https clone url in ci

--- meta/scripts/clone-quickjs.sh
+++ meta/scripts/clone-quickjs.sh
@@ -4,7 +4,11 @@ set -ex
 cd meta
 
 if [ ! -e 'quickjs' ]; then
-  git clone git@github.com:suchipi/quickjs.git
+  if [[ "${CI:-}" != "" ]]; then
+    git clone https://github.com/suchipi/quickjs.git
+  else
+    git clone git@github.com:suchipi/quickjs.git
+  fi
 fi
 
 pushd quickjs > /dev/null
