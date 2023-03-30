5082694ac0e366b9c168c5c2308b2a2063473400
Lily Skye <me@suchipi.com>
2023-03-29 20:56:39 -0600

    add runtime type assertions for arguments of parseScriptArgs, and change parseScriptArgs to use Path class for Path hint instead of the one from clef-parse

--- src/api/parse-script-args.ts
+++ src/api/parse-script-args.ts
@@ -1,26 +1,78 @@
 import * as clefParse from "clef-parse";
 import { Path } from "./path";
 import { pwd } from "./commands/pwd";
+import { assert } from "./assert";
+import { types } from "./types";
+import { makeErrorWithProperties } from "../error-with-properties";
 
-function parseScriptArgs(
-  hints: { [key: string]: clefParse.Hint } = {},
+type Hint = typeof String | typeof Boolean | typeof Number | typeof Path;
+
+export default function parseScriptArgs(
+  hints: { [key: string]: Hint } = {},
   args: Array<string> = scriptArgs.slice(2)
 ): {
   flags: { [key: string]: any };
   args: Array<string>;
 } {
+  assert.type(
+    hints,
+    types.anyObject,
+    "when present, 'hints' argument must be an object"
+  );
+
+  const hintsForClef: { [key: string]: clefParse.Hint } = {};
+  for (const [key, value] of Object.entries(hints)) {
+    if (typeof key !== "string") {
+      throw makeErrorWithProperties(
+        "all properties of 'hints' argument must be strings, but it was something else.",
+        { actual: key },
+        TypeError
+      );
+    }
+
+    switch (value) {
+      case String:
+      case Boolean:
+      case Number: {
+        hintsForClef[key] = value;
+        break;
+      }
+
+      case Path: {
+        hintsForClef[key] = clefParse.Path;
+        break;
+      }
+
+      default: {
+        throw makeErrorWithProperties(
+          `property '${key}' of 'hints' argument should be String, Boolean, Number, or Path, but it was something else.`,
+          { actual: value },
+          TypeError
+        );
+      }
+    }
+  }
+
+  assert.type(
+    args,
+    types.arrayOf(types.string),
+    "when present, 'args' argument must be an array of strings"
+  );
+
   // clef-parse calls `shift` on this
   const argsClone = args.slice();
 
-  const { options, positionalArgs } = clefParse.parseArgv(argsClone, hints, {
-    isAbsolute: Path.isAbsolute,
-    resolvePath: Path.resolve,
-    getCwd: pwd,
-  });
+  const { options, positionalArgs } = clefParse.parseArgv(
+    argsClone,
+    hintsForClef,
+    {
+      isAbsolute: Path.isAbsolute,
+      resolvePath: Path.resolve,
+      getCwd: pwd,
+    }
+  );
   return {
     flags: options,
     args: positionalArgs,
   };
 }
-
-export default Object.assign(parseScriptArgs, { Path: clefParse.Path });
