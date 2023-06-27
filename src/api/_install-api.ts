d18d7966e6872db0a1f187a0c50c0d6e369ef027
Lily Skye <me@suchipi.com>
2023-06-27 00:04:02 -0600

    add help text for standard ecmascript globals

--- src/api/_install-api.ts
+++ src/api/_install-api.ts
@@ -7,6 +7,7 @@ import { install as installRegexpEscape } from "./regexp-escape";
 import { installModuleHooks } from "../module-hooks";
 import { installNodeCompat } from "./node-compat/node-compat";
 import { patchRequire } from "../cjs-interop";
+import { installMdnLinkHelpTexts } from "../builtins-help-text/install-mdn-link-help-texts";
 
 const quickjsBuiltinsProps = makeGetterPropertyDescriptorMap({
   std: () => require("quickjs:std"),
@@ -207,4 +208,5 @@ export default function installApi(target: typeof globalThis) {
   installModuleHooks((target as any).Module);
   installNodeCompat(target);
   patchRequire(target);
+  installMdnLinkHelpTexts(target);
 }
