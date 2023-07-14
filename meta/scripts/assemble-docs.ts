6fd60b9e9c10cfd72087eb55298091f70a919f83
Lily Skye <me@suchipi.com>
2023-07-14 17:20:28 -0600

    make ys bin available to ninja via PATH

--- meta/scripts/assemble-docs.ts
+++ meta/scripts/assemble-docs.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env yavascript
 /// <reference path="../../yavascript.d.ts" />
+import * as os from "quickjs:os";
 
 const rootDir = GitRepo.findRoot(__dirname);
 cd(rootDir);
@@ -21,6 +22,22 @@ if (newBuildNinjaContent !== existingBuildNinjaContent) {
   writeFile(docsBuildNinjaPath, newBuildNinjaContent);
 }
 
+let ysBin = os.execPath();
+try {
+  ysBin = realpath(ysBin);
+} catch (err) {
+  console.warn(
+    "error when running realpath on result of os.execPath; ignoring",
+    err
+  );
+}
+
+const ysBinDir = dirname(ysBin);
+
 exec(["ninja", "-f", docsBuildNinjaPath.toString()], {
   cwd: rootDir.toString(),
+  env: {
+    ...env,
+    PATH: [ysBinDir, env.PATH].join(Path.OS_ENV_VAR_SEPARATOR),
+  },
 });
