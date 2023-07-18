3cd0ba6b2a0de0acae2121063de0a698010c0722
Lily Skye <me@suchipi.com>
2023-07-18 00:08:13 -0600

    more un-bootstrapifying

---
+++ meta/scripts/assemble-tgz.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd "$SCRIPT_DIR/../.." # go to root
+
+VERSION=`node -e 'console.log(require("./src/hardcoded/compile-time.js").version)'`
+
+if test -e meta/tgz-release; then
+  rm -rf meta/tgz-release
+fi
+mkdir -p meta/tgz-release
+cd meta/tgz-release
+
+TARGETS=(
+  aarch64-apple-darwin
+  aarch64-unknown-linux-gnu
+  aarch64-unknown-linux-musl
+  aarch64-unknown-linux-static
+  x86_64-apple-darwin
+  x86_64-pc-windows-static
+  x86_64-unknown-linux-gnu
+  x86_64-unknown-linux-musl
+  x86_64-unknown-linux-static
+)
+for TARGET in "${TARGETS[@]}"; do
+  mkdir -p "./$TARGET/yavascript-$VERSION"
+
+  cp ./bin/"$TARGET"/* "./$TARGET/yavascript-$VERSION"
+  cp ./dist/yavascript.d.ts "./$TARGET/yavascript-$VERSION/"
+
+  pushd "$TARGET" > /dev/null
+    tar -czvf "../yavascript-$VERSION-$TARGET.tar.gz" "yavascript-$VERSION/"
+  popd > /dev/null
+done
