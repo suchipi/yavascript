4cec80c12cbb5c95fa3ca569aa219b54417a595b
Lily Skye <me@suchipi.com>
2023-11-10 15:52:37 -0700

    move fnm use and npm install into build.sh instead of ninja-build.sh

--- meta/build.sh
+++ meta/build.sh
@@ -4,4 +4,14 @@ set -ex
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$SCRIPT_DIR/.." # go to root
 
+# Use node 18
+if [[ "${SKIP_FNM_USE:-}" == "" ]]; then
+  fnm use
+fi
+
+# grab JS dependencies from npm
+if [[ "${SKIP_NPM_INSTALL:-}" == "" ]]; then
+  npm install
+fi
+
 meta/scripts/ninja-build.sh
--- meta/scripts/ninja-build.sh
+++ meta/scripts/ninja-build.sh
@@ -6,16 +6,6 @@ shopt -s globstar
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$SCRIPT_DIR/../.." # go to root
 
-# Use node 18
-if [[ "${SKIP_FNM_USE:-}" == "" ]]; then
-  fnm use
-fi
-
-# grab JS dependencies from npm
-if [[ "${SKIP_NPM_INSTALL:-}" == "" ]]; then
-  npm install
-fi
-
 # this file needs to exist for the module resolve step in bundles.ninja.js
 # to work
 mkdir -p dist
