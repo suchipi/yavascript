84cfdfa6dfec976358d9d5514c41a3abc5a563fc
Lily Skye <git@suchipi.tv>
2025-04-01 15:20:09 -0600

    CI: run typecheck after tests so dts.test.ts is actually useful

--- meta/scripts/ci.sh
+++ meta/scripts/ci.sh
@@ -15,7 +15,7 @@ nvm install
 nvm use
 
 env SKIP_FNM_USE=1 meta/build.sh
-npm run typecheck
 # commented out until I can get it working locally
 # meta/docker/wine-test-image/build.sh
 npm test
+npm run typecheck
