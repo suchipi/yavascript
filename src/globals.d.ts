17d3a947af47385b9bdec78f4075b5cb1e74ec93
Lily Skye <git@suchipi.tv>
2026-07-13 20:50:14 -0600

    fix typescript error

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -7,4 +7,5 @@ declare var __yavascript_layer1_internals: import("./layer1/index").__yavascript
 declare var __yavascript_layer2_internals: import("./layer2/index").__yavascript_layer2_internals;
 declare var __yavascript_layer4_internals: import("./layer4/index").__yavascript_layer4_internals;
 
-declare var Worker: typeof import("quickjs:os").Worker;
+declare var Worker: typeof import("./layer3/Worker").Worker;
+declare var Context: typeof import("./layer3/context").Context;
